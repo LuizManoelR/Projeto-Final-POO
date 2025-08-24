948dedf0e430e6d85655a33f0613145b56ce50f9
mascneto <mascarenhasneto00@gmail.com>
2025-08-24 17:45:22 -0300

    fix GarcomeletronicoApplication, GarciomeletronicoApplicationTestes

---
+++ garcomeletronico/src/test/java/br/ufs/garcomeletronico/GarcomeltronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufs.garcomeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import br.ufs.garcomeletronico.app.GarcomeletronicoApplication;
+
+@SpringBootTest(classes = GarcomeletronicoApplication.class)
+class GarcomeletronicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
