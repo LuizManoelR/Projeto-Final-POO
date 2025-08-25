ff12260be22501ff7053997fcf4c92c188273645
LuizManoelR <luiz.meneses@dcomp.ufs.br>
2025-08-25 16:07:45 -0300

    Create CarrinhoService.java
    Responsável pelo gereciamento de carrinho para iniciar, adcionar, remover,finalizar ou listar o carrinho. Aleém disso salva qualquer alteração por meio do CarrinhoCookieService

---
+++ garcomeletronico/src/main/java/br/ufs/garcomeletronico/service/CarrinhoService.java
@@ -0,0 +1,81 @@
+package br.ufs.garcomeletronico.service;
+
+import java.util.List;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Service;
+
+import br.ufs.garcomeletronico.model.Carrinho;
+import br.ufs.garcomeletronico.model.Item;
+import br.ufs.garcomeletronico.model.Mesa;
+import br.ufs.garcomeletronico.model.Produto;
+
+
+@Service
+public class CarrinhoService {
+
+    CarrinhoCookieService carrinhoCookieService;
+    PedidoService pedidoService;
+    ComandaService comandaService;
+
+    public CarrinhoService(CarrinhoCookieService carrinhoCookieService, 
+                           PedidoService pedidoService, 
+                           ComandaService comandaService){
+
+        this.carrinhoCookieService = carrinhoCookieService;
+        this.pedidoService = pedidoService;
+        this.comandaService = comandaService;
+    }
+
+    public void iniciar(HttpServletResponse response,Mesa mesa){
+        
+        carrinhoCookieService.salvar(response, new Carrinho(mesa));
+
+    }
+
+    public void adcionar(HttpServletRequest request,HttpServletResponse response,Produto produto){
+
+        Carrinho carrinho = carrinhoCookieService.carregar(request);
+
+        carrinho.add(produto);
+
+        carrinhoCookieService.salvar(response, carrinho);
+
+    }
+    public void remover(HttpServletRequest request,HttpServletResponse response,Produto produto){
+
+        Carrinho carrinho = carrinhoCookieService.carregar(request);
+
+        carrinho.remove(produto);
+
+        carrinhoCookieService.salvar(response, carrinho);
+
+    }
+    
+    public List<Item> listar(HttpServletRequest request){
+
+        Carrinho carrinho = carrinhoCookieService.carregar(request);
+
+        return carrinho.getCarrinho();
+
+    }
+
+
+
+    public void finalizar(HttpServletRequest request,HttpServletResponse response, String id){
+
+        Carrinho carrinho = carrinhoCookieService.carregar(request);
+
+        comandaService.adicionarItem(id,carrinho.getCarrinho());
+        pedidoService.criarPedido(carrinho.getCarrinho(), carrinho.getMesa());
+
+        carrinho.esvaziar();
+        
+        carrinhoCookieService.salvar(response, carrinho);
+
+    }
+
+
+}
